917b6884393e360021d21f8e8b55ee25c700abd4
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-17 15:59:47 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Gabriel Marquez on 2022-06-17.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
